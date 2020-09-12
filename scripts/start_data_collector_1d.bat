07077516e99f7e044e2cdb04889da86c42125816
Seungyong Bae <catastro728@gmail.com>
2020-09-12 16:00:21 +0900

    Add scheduler setting, code

---
+++ scripts/start_data_collector_1d.bat
@@ -0,0 +1,2 @@
+cd C:\Users\catas\Project\auto-stock-trading-system
+C:\Users\catas\miniconda3\envs\autostock\Scripts\python.exe -m stocklab.scheduler.data_collector_1d %
--- tests/test_agent_ebest.py
+++ tests/test_agent_ebest.py
@@ -50,3 +50,43 @@ class TestEbest(unittest.TestCase):
         result = self.ebest.get_agent_trend_by_code("005930", fromdt="20200822", todt="20200822")
         assert result is not None
         print(result)
+
+    def test_get_account_info(self):
+        result = self.ebest.get_account_info()
+        assert result is not None
+        print(result)
+    
+    def test_get_account_stock_info(self):
+        result = self.ebest.get_account_stock_info()
+        assert result is not None
+        print(result)
+
+    def test_order_stock(self):
+        result = self.ebest.order_stock("005930", "2", "50000", "2", "00")
+        assert result
+        print(result)
+
+    def test_order_cancel(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.order_cancel("29515", "A005930", "2")
+        assert result
+        print(result)
+
+    def test_order_check(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.order_check("29515")
+        assert result
+        print(result)
+
+    def test_get_current_call_price_by_code(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.get_current_call_price_by_code("005930")
+        assert result
+        print(result)
+
+    def test_get_tick_size(self):
+        print(inspect.stack()[0][3])
+        price = self.ebest.get_current_call_price_by_code("005930")
+        result = self.ebest.get_tick_size(int(price[0]["현재가"]))
+        assert result
+        print("호가 단위 : " + str(result))
